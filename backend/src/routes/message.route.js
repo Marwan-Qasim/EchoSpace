1e0f8a7461e1cba1dcb747ac43fc6ecdbbc8a997
voisnoa <markzlopp@gmail.com>
2025-11-12 11:09:41 +0530

    change type to modules

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,8 @@
-const express = require('express');
-
+import express from 'express';
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-    res.send('Message endpoint');
+router.get('/', (req, res) => {
+    res.send('Auth route working');
 });
 
-module.exports = router;
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const authRoute = require('./routes/auth.route.js');
-const messageRoute = require('./routes/message.route.js');
-const path = require('path');
-
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import authRoute from './routes/auth.route.js';
+import messageRoute from './routes/message.route.js';
 
 dotenv.config();
-const __dirname = path.resolve();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
 }
 
-app.use("/api/auth", authRoute);
-app.use("/api/messages", messageRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/messages', messageRoute);
 
 app.listen(PORT, () => {
-    console.log('Server is running on port',PORT);
+    console.log('Server is running on port', PORT);
 });
