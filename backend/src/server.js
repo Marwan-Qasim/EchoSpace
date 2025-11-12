51a8e01edb2c5d7e3ab2254725d33be58a72b202
voisnoa <markzlopp@gmail.com>
2025-11-12 10:07:38 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth.route.js');
 const messageRoute = require('./routes/message.route.js');
+const path = require('path');
+
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (_, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
